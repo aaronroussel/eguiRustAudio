fbed9fa77259e7db3358f76f3d2c1867a54666c9
aaronroussel <84491515+aaronroussel@users.noreply.github.com>
2023-09-07 20:46:34 -0500

    Side Panel; Add from file path
    added mock side panel design for playlist functionality (not functional currently)
    
    added ability to specify a path to a folder and add music from that folder. Accessed through the top menu. file>add music folder

--- src/application/file_handling/file_handling.rs
+++ src/application/file_handling/file_handling.rs
@@ -15,7 +15,8 @@ pub struct music_file {
 pub fn get_library() -> Vec<music_file> {
     let mut music_files: Vec<music_file> = Vec::new();
     
-    let paths = fs::read_dir("C:\\Users\\aaron\\Music\\songs").unwrap(); // THIS FOLDER MUST ONLY CONTAIN MUSIC FILES OR SHIT WILL BREAK!!!
+    // let paths = fs::read_dir("C:\\Users\\aaron\\Music\\songs").unwrap(); // THIS FOLDER MUST ONLY CONTAIN MUSIC FILES OR SHIT WILL BREAK!!!
+    let paths = fs::read_dir("/Users/aaronroussel/Music/Music/Media.localized/Music/Future/Unknown Album").unwrap();
     
     for p in paths {
         let dir_entry = p.unwrap();
@@ -38,6 +39,38 @@ pub fn get_library() -> Vec<music_file> {
         
         music_files.push(music);
     }
+    
+    music_files
+}
+
+pub fn get_from_path(path_string: &str) -> Vec<music_file> {
+    
+    let mut music_files: Vec<music_file> = Vec::new();
+    
+    let paths = fs::read_dir(path_string).unwrap();
+    
+    for p in paths {
+        let dir_entry = p.unwrap();
+        let tag = Tag::new().read_from_path(dir_entry.path().clone()).unwrap();
+        
+        
+        let song_title = tag.title().map(|s| s.to_string()).unwrap_or_default();
+        let song_artist = tag.artists().map(|artists| artists.join(", ")).unwrap_or_default();
+        let song_duration = tag.duration().unwrap_or_default(); 
+        let song_album = tag.album_title().map(|s| s.to_string()).unwrap_or_default();
+        
+        let music = music_file {
+            name: dir_entry.path().file_name().unwrap().to_string_lossy().to_string(),
+            file_path: dir_entry.path().clone(),
+            title: song_title,
+            duration: song_duration,
+            artist: song_artist,
+            album: song_album,
+        };
+        
+        music_files.push(music);
+    }
+    
     
     music_files
 }
